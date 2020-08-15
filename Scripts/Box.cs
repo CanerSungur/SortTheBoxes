c2c485f48321d36eb10e4797f183593aa09e7915
CanerSungur <caner.sokur@outlook.com>
2020-08-15 21:14:09 +0300

    Create Box.cs

---
+++ Scripts/Box.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+	public GameObject boxPrefab;
+	public Sprite boxSprite;
+	public string boxName;
+	public int typeNo;
+	public bool isBoxOnInnerConveyor = false;
+}
